4e960d89c0cb878ddc2efd7bf11e76b7f33c0e76
Valeriy Kozlov <Valera.31.07.9.9@gmail.com>
2023-05-29 15:36:31 +0300

    default image

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -11,10 +11,9 @@ export class JwtAuthGuard implements CanActivate {
         const req = context.switchToHttp().getRequest()
         try {
             const authHeader = req.headers.authorization;
-            const bearer = authHeader.split(' ')[0]
-            const token = authHeader.split(' ')[1]
+            const token = authHeader;
 
-            if (bearer !== 'Bearer' || !token) {
+            if (!token) {
                 throw new UnauthorizedException({ message: 'not authorized' })
             }
 
